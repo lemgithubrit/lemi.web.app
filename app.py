39dac729b5a2b0677656a3dca8b2f3af62184e7e
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2022-06-26 03:38:36 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ st.set_page_config(page_title="Wabepage", page_icon=":tada:", layout="wide")
 st.title("Engineering Properties of Ethiopian Grains Crops")
 st.write("Five major cereals (Teff, Wheat, Maize, Sorghum, and Barley)"
          )
-
 #subheader
 with st.container():
     st.write("---") 
@@ -23,14 +22,8 @@ df = pd.DataFrame({
   'Bulk Density': [1, 20, 30, 40, 50],
   'Solid Density': [30, 20, 40, 40, 90],
   'Angle of Repose': [3, 2, 40, 10,9]
-
 })
 df
-
-
-
-
-
 #sidenotes 
 st.sidebar.title("Option")
 text=st.sidebar.text_area("Paste Text Here")
@@ -38,11 +31,8 @@ st.write(text)
 button1=st.sidebar.button("Add Text")
 date = st.sidebar.date_input("Pick a date")
 #add file
-
-#gap
 st.write("##")
 st.write("##")
-
 #authenticator 
 st.subheader("Registration")
 frist,second,last=st.columns(3)
@@ -57,28 +47,10 @@ user.text_input("Username")
 pw.text_input("password",type="password")
 pw2.text_input("Retype your password",type="password")
 button1=st.button("Submit")
-
-
-
-
-
 st.write("##")
 st.write("##")
 st.write("##")
 st.write("##")
-
-
 #footer 
 st.caption('streamlit turns data scripts into shareable web appin minutes All in pure python No front end experience required')
 st.write("Developer(Lemi Demissie PhD student @ASTU")
-
-
-
-
-
-
-
-
-
-
-
